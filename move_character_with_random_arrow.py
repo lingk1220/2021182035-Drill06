97ad865933da122bde07632173fae190403deb8c
lingk4231075 <strbryjam1220@gmail.com>
2024-10-03 17:27:39 +0900

    점에 도착하면 랜덤 점 재지정 구현

--- move_character_with_random_arrow.py
+++ move_character_with_random_arrow.py
@@ -1,3 +1,5 @@
+from random import random, randint
+
 from pico2d import *
 import math
 
@@ -13,14 +15,13 @@ character = load_image('animation_sheet.png')
 def handle_events():
     global running
 
-    global x, y
 
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, TUK_HEIGHT - 1 - event.y
+            pass
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
@@ -28,8 +29,11 @@ def run_destination():
     global pos_character, destination
 
     if 1 > get_dist(pos_character, destination):
-        pos_character = destination
+        pos_character[0] = destination[0]
+        pos_character[1] = destination[1]
         reset_destination()
+        print(pos_character)
+        print(destination)
     else:
         pos_character[0] += (destination[0] - pos_character[0]) / 30
         pos_character[1] += (destination[1] - pos_character[1]) / 30
@@ -43,6 +47,9 @@ def get_dist(pos_character, destination):
     return math.sqrt(math.pow(dx - cx, 2) + math.pow(dy - cy, 2))
 
 def reset_destination():
+    destination[0] = randint(10, TUK_WIDTH - 10 + 1)
+    destination[1] = randint(10, TUK_HEIGHT - 10 + 1)
+
     pass
 
 running = True
@@ -59,6 +66,7 @@ while running:
     character.clip_draw(frame * 100, 100 * 1, 100, 100, pos_character[0], pos_character[1])
     update_canvas()
     run_destination()
+
     frame = (frame + 1) % 8
     delay(0.01)
     handle_events()
